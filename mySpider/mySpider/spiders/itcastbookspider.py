be080096221490bc9618fc733b08c1b711eb79a5
seizeMiss <dragon_csu@163.com>
2020-06-01 14:37:55 +0800

    修改自写爬虫出错的问题

--- mySpider/mySpider/spiders/itcastbookspider.py
+++ mySpider/mySpider/spiders/itcastbookspider.py
@@ -6,28 +6,31 @@ from mySpider.items import ItcastBookItem
 
 class ItcastBookSpider(scrapy.Spider):
     name = 'itcase_book_spider'
-    allowd_domain = ["http://resource.boxuegu.com/"]
+    allowd_domain = ["resource.boxuegu.com/"]
     start_urls = [
-        "http://resource.boxuegu.com/booklist/",
+        "http://resource.boxuegu.com/booklist/find.html",
     ]
 
     def parse(self, response):
-        book_list = response.xpath('//div[@class="book-home-panel-book"]')
-
+        book_list = response.xpath('//div[@class="row"]')
         bookItems = []
+        print(book_list)
 
         for item in book_list:
+
             bookItem = ItcastBookItem()
-            name = item.xpath('./div[@class="col-md-7"]/div/div/p[@class="book-list-s-title"]/a/text()').extract()
-            version = item.xpath(
-                './div[@class="col-md-7"]/div/div/p[@class="book-list-s-book-list-s-press"]/text()').extract()
-            ISBN = item.xpath('./div[@class="col-md-7"]/div/div/p['
-                              '@class="book-list-s-book-list-s-press"]/following-sibling::p[1]/text()').extract()
-            price = item.xpath('./div[@class="col-md-7"]/p/span[@class="book-list-money"]/text()').extract()
+            name = item.xpath('./div[2]/p[@class="book-list-s-title"]/a/text()').extract()
+            version = item.xpath('./div[2]/p[@class="book-list-s-press"]/text()').extract()
+            ISBN = item.xpath('./div[2]/p[@class="book-list-s-isbn"]/text()').extract()
+            price = item.xpath('./div[4]/p/span[@class="book-list-money"]/text()').extract()
 
             bookItem['name'] = name[0].encode("utf8")
             bookItem['version'] = version[0].encode("utf8")
             bookItem['ISBN'] = ISBN[0].encode("utf8")
+            if len(price) == 0:
+                bookItem['price'] = ""
+                bookItems.append(bookItem)
+                continue
             bookItem['price'] = price[0].encode("utf8")
             bookItems.append(bookItem)
 
